03fb8eedb78a4f243d958eead3470b05cd43e219
Jacob Iglesias <jacob.ds.iglesias.5@gmail.com>
2020-10-02 12:08:32 -0400

    Add project files.

---
+++ ChangeCalculator/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace ChangeCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbIn = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbP = new System.Windows.Forms.TextBox();
+            this.tbN = new System.Windows.Forms.TextBox();
+            this.tbD = new System.Windows.Forms.TextBox();
+            this.tbQ = new System.Windows.Forms.TextBox();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Amount of change due (0-99):";
+            // 
+            // tbIn
+            // 
+            this.tbIn.Location = new System.Drawing.Point(167, 10);
+            this.tbIn.Name = "tbIn";
+            this.tbIn.Size = new System.Drawing.Size(26, 20);
+            this.tbIn.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.tbP);
+            this.groupBox1.Controls.Add(this.tbN);
+            this.groupBox1.Controls.Add(this.tbD);
+            this.groupBox1.Controls.Add(this.tbQ);
+            this.groupBox1.Location = new System.Drawing.Point(13, 30);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(182, 126);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "U.S. Coins";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(96, 100);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Pennies:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(99, 74);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Nickels:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(105, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Dimes:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(94, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Quarters:";
+            // 
+            // tbP
+            // 
+            this.tbP.Location = new System.Drawing.Point(150, 97);
+            this.tbP.Name = "tbP";
+            this.tbP.ReadOnly = true;
+            this.tbP.Size = new System.Drawing.Size(26, 20);
+            this.tbP.TabIndex = 6;
+            this.tbP.TabStop = false;
+            // 
+            // tbN
+            // 
+            this.tbN.Location = new System.Drawing.Point(150, 71);
+            this.tbN.Name = "tbN";
+            this.tbN.ReadOnly = true;
+            this.tbN.Size = new System.Drawing.Size(26, 20);
+            this.tbN.TabIndex = 3;
+            this.tbN.TabStop = false;
+            // 
+            // tbD
+            // 
+            this.tbD.Location = new System.Drawing.Point(150, 45);
+            this.tbD.Name = "tbD";
+            this.tbD.ReadOnly = true;
+            this.tbD.Size = new System.Drawing.Size(26, 20);
+            this.tbD.TabIndex = 2;
+            this.tbD.TabStop = false;
+            // 
+            // tbQ
+            // 
+            this.tbQ.Location = new System.Drawing.Point(150, 19);
+            this.tbQ.Name = "tbQ";
+            this.tbQ.ReadOnly = true;
+            this.tbQ.Size = new System.Drawing.Size(26, 20);
+            this.tbQ.TabIndex = 1;
+            this.tbQ.TabStop = false;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCalc.Location = new System.Drawing.Point(12, 162);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(126, 23);
+            this.btnCalc.TabIndex = 1;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(144, 162);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(52, 23);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnCalc;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(207, 196);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.tbIn);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "Change Calculator";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbIn;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbP;
+        private System.Windows.Forms.TextBox tbN;
+        private System.Windows.Forms.TextBox tbD;
+        private System.Windows.Forms.TextBox tbQ;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ ChangeCalculator/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChangeCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            // try to get int input
+            int usrin;
+            try
+            {
+                usrin = Convert.ToInt32(tbIn.Text);
+            }
+            catch (Exception) {
+                MessageBox.Show("Invaid Input");
+
+                return;
+            }
+
+            // do calculation
+            int amount = 0; // reused var
+
+            // quarters
+            amount = 0;
+            while (usrin > 24) {
+                amount++;
+                usrin -= 25;
+            }
+            tbQ.Text = Convert.ToString(amount);
+
+            // dimes
+            amount = 0;
+            while (usrin > 9)
+            {
+                amount++;
+                usrin -= 10;
+            }
+            tbD.Text = Convert.ToString(amount);
+
+            // nickels
+            amount = 0;
+            while (usrin > 4)
+            {
+                amount++;
+                usrin -= 5;
+            }
+            tbN.Text = Convert.ToString(amount);
+
+            // pennies
+            amount = 0;
+            while (usrin > 0)
+            {
+                amount++;
+                usrin -= 1;
+            }
+            tbP.Text = Convert.ToString(amount);
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
